af2e820f10af4781df8e55c05a79864a005817eb
Sigrid Solveig Haflínudóttir <sigrid@ftrv.se>
2024-10-06 01:32:46 +0200

    e* utils: add attributes to avoid analyzers assuming funcs can return NULL

--- util.h
+++ util.h
@@ -1,5 +1,5 @@
-void	*emalloc(size_t);
-void	*erealloc(void*, size_t);
-void	*ecalloc(size_t, size_t);
-char	*estrdup(const char *);
+void	*emalloc(size_t) __attribute__((malloc, returns_nonnull));
+void	*erealloc(void*, size_t) __attribute__((returns_nonnull));
+void	*ecalloc(size_t, size_t) __attribute__((malloc, returns_nonnull));
+char	*estrdup(const char *) __attribute__((malloc, returns_nonnull));
 void	dprint(char*, ...);
